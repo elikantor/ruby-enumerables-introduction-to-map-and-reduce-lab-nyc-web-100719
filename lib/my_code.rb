8bd97cbe7440a60d2bf69a8f23ea75fcf43edecb
Eli Kantor <eli.kantor@outlook.com>
2019-09-29 22:58:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -74,7 +74,22 @@ def reduce_to_total(source_array, starting_point = 0)
   end
   
   return result
-  
 
 end
+
+def reduce_to_all_true(source_array)
+  
+  i = 0 
+
+  while i < source_array.length 
+    if !source_array[i]
+      return false 
+    end
+    i += 1 
+  end
+  
+  return true 
+
+end
+
 
