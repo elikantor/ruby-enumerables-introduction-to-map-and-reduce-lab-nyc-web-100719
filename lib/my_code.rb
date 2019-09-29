f37bc7bafad8e54c2bf458015396f1a07600fe66
Eli Kantor <eli.kantor@outlook.com>
2019-09-29 22:38:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# My Code here....
+def map_to_negativize(source_array)
+
+  i = 0 
+  newarr = []
+  
+  while i < source_array.length 
+    newarr << (source_array[i] * -1)
+    
+    i += 1 
+  end
+  
+  return newarr
+
+end
