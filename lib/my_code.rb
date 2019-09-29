8de52f5c896adb471ee97e67eda20596e92444ae
Eli Kantor <eli.kantor@outlook.com>
2019-09-29 22:55:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -65,7 +65,7 @@ end
 def reduce_to_total(source_array, starting_point = 0)
   
   i = 0 
-  result = 0 
+  result = starting_point
   
   while i < source_array.length 
     result += source_array[i]
