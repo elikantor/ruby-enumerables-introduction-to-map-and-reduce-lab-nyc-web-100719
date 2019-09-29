dd404b477918ec651cbb44743a1654b0eae4f02a
Eli Kantor <eli.kantor@outlook.com>
2019-09-29 22:51:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,3 +29,35 @@ def map_to_no_change(source_array)
   
 
 end
+
+def map_to_double(source_array)
+  
+  i = 0 
+  newarr = []
+  
+  while i < source_array.length 
+    newarr << (source_array[i] *)
+    
+    i += 1 
+  end
+  
+  return newarr
+  
+
+end
+
+def map_to_squaree(source_array)
+  
+  i = 0 
+  newarr = []
+  
+  while i < source_array.length 
+    newarr << (source_array[i] ** 2)
+    
+    i += 1 
+  end
+  
+  return newarr
+  
+
+end
